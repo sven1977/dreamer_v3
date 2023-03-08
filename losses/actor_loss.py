2 BIG BUG FIXES: * Sampled actions need to be stop_gradient'd when arriving as part of the trajectory inside the actor loss. For now, only supporting discrete actions, this wouldn't matter, b/c the sampling step is zero-gradient anyways. But for when we switch to straight-through action sampling (for cont. actions), this would become a problem. * Value targets must be stop_gradient'd before using them in the critic loss! The actor loss was already correctly stop_gradient'ing the scaled value targets, though.

losses/actor_loss.py
@@ -16,6 +16,8 @@ def actor_loss(
     entropy_scale,
     return_normalization_decay,
 ):
+    # Note: `value_targets` are already stop_gradient'd. The returned
+    # `scaled_value_targets` as well.
     scaled_value_targets_t0_to_Hm1_B = compute_scaled_value_targets(
         value_targets=value_targets,  # targets are already [t0 to H-1, B]
         value_predictions=dream_data["values_dreamed_t0_to_H_B"][:-1],
@@ -24,7 +26,7 @@ def actor_loss(
     )
 
     # Actions actually taken in the dream.
-    actions_dreamed = dream_data["actions_dreamed_t0_to_H_B"][:-1]
+    actions_dreamed = tf.stop_gradient(dream_data["actions_dreamed_t0_to_H_B"][:-1])
     # Log(p)s of all possible actions in the dream.
     # Note that when we create the Categorical action distributions, we compute
     # unimix probs, then math.log these and provide these log(p) as "logits" to the